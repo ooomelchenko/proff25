5b6ecbe2f9c45ca03fa9f74528d28ce85aef94f8
alexandr_omelchenko <ooomelchenko@gmail.com>
2016-11-06 18:56:58 +0200

    update test

--- alex_nb_web/src/main/java/nadrabank/controller/Test.java
+++ alex_nb_web/src/main/java/nadrabank/controller/Test.java
@@ -5,7 +5,7 @@ import java.math.BigDecimal;
 public class Test {
     public static void main(String[] args) {
         BigDecimal a = new BigDecimal(80);
-        BigDecimal b = new BigDecimal(30.5);
+        BigDecimal b = new BigDecimal(31.5);
          b = b.add(a);
         System.out.println(b);
     }
